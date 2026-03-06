14dd1556ab79db50fccb0e838730527b30a84d36
Jake Gendreau <jake.lgendreau@gmail.com>
2026-03-05 16:40:13 -0800

    Lab project 2 done

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 var ready = (callback) => {
     if (document.readyState != "loading") callback();
     else document.addEventListener("DOMContentLoaded", callback);
+    highlightCurrentPageButton();
 }
 ready(() => {
-    document.querySelector(".header").style.height = window.innerHeight + "px";
+    try {
+        document.querySelector(".header").style.height = window.innerHeight + "px";
+    } catch (error) {
+        
+    }
 })
+
+function highlightCurrentPageButton()
+{
+    const curPath = document.URL;
+    const curPage = curPath.split("/")[3].split(".")[0];
+
+    var navButton = document.getElementById(curPage);
+
+    console.log(navButton);
+    if(!navButton)
+    {
+        return;
+    }
+
+    navButton.classList.add("current-page");
+}
